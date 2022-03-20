08fdb7645aef056cb38043345f03e9d8f4476ab4
goshgarhasanov <goshgar544@gmail.com>
2022-03-20 11:28:17 +0400

    Repo Init

---
+++ run.py
@@ -0,0 +1,10 @@
+from flask import Flask,jsonify
+import requests
+app=Flask(__name__)
+posts=requests.get("https://jsonplaceholder.typicode.com/posts").json()
+@app.get("/posts")
+def getUsers():
+    return jsonify(posts)
+
+if __name__=="__main__":
+    app.run(debug=1)
